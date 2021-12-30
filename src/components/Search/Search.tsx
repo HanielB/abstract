23f135ba7a7f361352932560176b6e84b6ad1707
Haniel Barbosa <hanielbbarbosa@gmail.com>
2021-12-30 17:27:04 -0300

    adding commented out version of handler of python code

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -4,6 +4,52 @@ import "./Search.css";
 import { searchMovies, getMovie, loadMovies } from "../../services/movies.service";
 import { MoviesContext } from "../../services/context";
 
+import script from './python/main.py';
+
+// const runScript = async (code) => {
+//   const pyodide = await window.loadPyodide({
+//     indexURL : "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/"
+//   });
+
+//   await pyodide.loadPackage("datetime");
+//   // await pyodide.loadPackage("micropip");
+
+//   // pyodide.runPythonAsync(`
+//   // import micropip
+//   // await micropip.install('python-datetutil')
+//   // `);
+//   await pyodide.loadPackage("python-dateutil");
+
+//   const watchedText =
+//         await (await fetch(`test.csv`,
+//                            {
+//                              method: 'get',
+//                              headers: {
+//                                'content-type': 'text/csv;charset=UTF-8',
+//                              }
+//                            })).text();
+//   console.log("Read from watched: " + watchedText)
+//   // const watchedCsv = await Papa.parse(watchedText);
+//   // console.log("Papa parsed: " + watchedCsv)
+
+//   const headers = watchedText.slice(0, watchedText.indexOf("\n")).split(",");
+//   const rowsFlat = watchedText.slice(watchedText.indexOf("\n") + 1).split("\n");
+
+//   const rows = rowsFlat.map((row) => row.split(","));
+
+//   console.log("Headers (" + headers.length + "): " + headers);
+//   console.log("Rows: (" + rows.length + "): " + rows.forEach((r) => console.log("\n\t" + r.length + ": " + r)));
+
+//   let my_js_namespace = { x : watchedText, w : rows };
+//   pyodide.registerJsModule("my_js_namespace", my_js_namespace);
+
+//   const res = await pyodide.runPythonAsync(code);
+//   console.log("Length of read: " + window.y);
+//   console.log("First array elem: " + window.z);
+//   return res;
+// }
+
+
 export const Search = () => {
   const [search, setSearch] = useState("");
   const [movieId, setMovieId] = useState("");

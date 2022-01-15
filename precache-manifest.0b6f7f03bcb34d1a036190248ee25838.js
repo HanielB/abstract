27682db85986eb5a1b0722eaf462566737f84f29
Haniel Barbosa <hanielbbarbosa@gmail.com>
2022-01-15 12:42:53 -0300

    test

---
+++ precache-manifest.0b6f7f03bcb34d1a036190248ee25838.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3800353bfc09d503ebb5fb41b26528",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ff00db8d7ae0c1e7f29",
+    "url": "/static/css/main.d799ece1.chunk.css"
+  },
+  {
+    "revision": "0819ac5f3b65acca2528",
+    "url": "/static/js/2.c7e30709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c7e30709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ff00db8d7ae0c1e7f29",
+    "url": "/static/js/main.3f87f1f0.chunk.js"
+  },
+  {
+    "revision": "466c1a7fdaa58576fa1f",
+    "url": "/static/js/runtime-main.7d4b7ba4.js"
+  }
+]);

d2262cab07701b86b0e1248eea88f814539280a1
Haniel Barbosa <hanielbbarbosa@gmail.com>
2022-01-15 20:56:11 -0300

    test

---
+++ precache-manifest.4ae03cba326e18f71159d54e94ec98e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835aaed3bac1b1d065bcf06cdbd5074b",
+    "url": "/movie-app-react/index.html"
+  },
+  {
+    "revision": "8b0a6b43e1ae30f532c3",
+    "url": "/movie-app-react/static/css/main.f47d26b0.chunk.css"
+  },
+  {
+    "revision": "53933d94934bdf7430c2",
+    "url": "/movie-app-react/static/js/2.528909d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app-react/static/js/2.528909d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0a6b43e1ae30f532c3",
+    "url": "/movie-app-react/static/js/main.49b9d5a7.chunk.js"
+  },
+  {
+    "revision": "9a5d1259a7d1093b5c4b",
+    "url": "/movie-app-react/static/js/runtime-main.ea39982e.js"
+  }
+]);
---
+++ precache-manifest.f44451b56277ddeedd5ce1322d0de389.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a200c58b72914ddab6c5f7709963da8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "92db9f06978348fb7a99",
+    "url": "./static/css/main.f47d26b0.chunk.css"
+  },
+  {
+    "revision": "53933d94934bdf7430c2",
+    "url": "./static/js/2.528909d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.528909d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92db9f06978348fb7a99",
+    "url": "./static/js/main.add81131.chunk.js"
+  },
+  {
+    "revision": "fe9175436a64724674af",
+    "url": "./static/js/runtime-main.c9a105a5.js"
+  }
+]);

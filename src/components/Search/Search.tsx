2a17282178fe2a09753e4776594c716d2018bd11
Haniel Barbosa <hanielbbarbosa@gmail.com>
2023-01-09 20:49:21 -0300

    minor

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -234,12 +234,12 @@ export const Search = () => {
               </div>
               <div className="form_radio">
                 <div>
-                  <input type="checkbox" id="available" name="display" value="available"/>
-                  <label htmlFor="available">Available</label>
-                </div>
-                <div>
                   <input type="checkbox" id="collection" name="display" value="collection"/>
                   <label htmlFor="collection">Collection</label>
+                </div>
+                <div>
+                  <input type="checkbox" id="available" name="display" value="available"/>
+                  <label htmlFor="available">Available</label>
                 </div>
               </div>
             </fieldset>

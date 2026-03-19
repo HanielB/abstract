b3b1c59e7a22fd033c3ab369d0cd50f1a04e520f
Haniel Barbosa <hanielbbarbosa@gmail.com>
2026-03-19 00:08:49 -0300

    go

--- upload.sh
+++ upload.sh
@@ -21,7 +21,7 @@ echo "Uploading lists..."
 ./index.py lists/
 rsync --exclude='.git/' --exclude='node-modules' --recursive --times --compress --progress lists hostinger:~/public_html/filminhos/ &> /dev/null
 echo "Uploading directors..."
-./directors.py
+python3 directors.py
 rsync --exclude='.git/' --exclude='node-modules' --recursive --times --compress --progress directors hostinger:~/public_html/filminhos/ &> /dev/null
 # echo "Uploading yearly review..."
 # rsync --exclude='.git/' --recursive --times --compress --progress year-review hostinger:~/public_html/filminhos/ &> /dev/null

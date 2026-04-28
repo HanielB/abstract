b857c3a9477657f429a3ddf06ba907f93ec8d6e2
Haniel Barbosa <hanielbbarbosa@gmail.com>
2026-04-27 21:36:17 -0300

    update review

--- upload.sh
+++ upload.sh
@@ -23,6 +23,6 @@ rsync --exclude='.git/' --exclude='node-modules' --delete --recursive --times --
 echo "Uploading directors..."
 python3 directors.py
 rsync --exclude='.git/' --exclude='node-modules' --recursive --times --compress --progress directors hostinger:~/public_html/filminhos/ &> /dev/null
-# echo "Uploading yearly review..."
-# rsync --exclude='.git/' --recursive --times --compress --progress year-review hostinger:~/public_html/filminhos/ &> /dev/null
+echo "Uploading yearly review..."
+rsync --exclude='.git/' --recursive --times --compress --progress year-review hostinger:~/public_html/filminhos/ &> /dev/null
 rm public/master.json
